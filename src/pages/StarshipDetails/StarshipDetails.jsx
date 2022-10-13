7c10e9b0fa8bc946a882c2f8f85b149f3a350a52
Leon Chu <Ljchu87@gmail.com>
2022-10-13 17:02:21 -0500

    Initialize starship details page

--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -1,12 +1,34 @@
 import { useState, useEffect } from "react";
+import { getDetails } from "../../services/api-calls";
+import { Link, useLocation } from "react-router-dom";
 
 
 const StarshipDetails = () => {
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const location = useLocation()
+  
+  useEffect(() => {
+    const fetchStarshipDetails = async () => {
+      const starshipData = await getDetails(location.state.starship.url)
+      setStarshipDetails(starshipData)
+    }
+    fetchStarshipDetails()
+  }, [location.state.starship.url])
+
   return (
     <>
-    <h3>This is details</h3>
-    </>
-  );
+    <h3>Starship Details</h3>
+    {starshipDetails.name ?
+      <>
+        <h3>Name: {starshipDetails.name}</h3>
+        <h3>Model: {starshipDetails.model}</h3>
+        <Link to = '/'>Return</Link>
+      </>
+      :
+      <p>Loading details...</p>
+    }
+  </>
+);
 }
 
 export default StarshipDetails;
--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,11 +1,11 @@
-const baseUrl = 'https://swapi.dev'
+const baseUrl = 'https://swapi.dev/api'
 
 export async function getStarshipsList() {
-  const res = await fetch(`${baseUrl}/api/starships`)
+  const res = await fetch(`${baseUrl}/starships`)
     return res.json()
 }
 
 export async function getDetails(apiUrl) {
-  const res = await fetch (`${baseUrl}${apiUrl}`)
+  const res = await fetch (`${apiUrl}`)
   return res.json()
 }
